5a6fa5bc65d36cee8fe09342314f2fe1f703e04b
Shrey K <srkkadia@gmail.com>
2024-08-30 13:06:54 +0530

    Thread Scheduler Theory

---
+++ MultiThreading/Joining_Thread.java
@@ -0,0 +1,33 @@
+class MyThread extends Thread {
+    public MyThread() {
+        super();
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + ": " + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Joining_Thread {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("One");
+        MyThread t2 = new MyThread("Two");
+        MyThread t3 = new MyThread("Three");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ MultiThreading/Thread_Scheduler.java
@@ -0,0 +1,5 @@
+public class Thread_Scheduler {
+    public static void main(String[] args) {
+        //
+    }
+}
